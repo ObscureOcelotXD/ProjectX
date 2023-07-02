c86c75478af6af27bc7f7292c308b1d467c2e864
Mitchell Spurgat <mspurgat93@gmail.com>
2023-07-01 20:23:31 -0500

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectXBusiness;
+using System;
+
+
+namespace ProjectX.Pages
+{
+    public class IndexModel : PageModel
+    {
+        //[Inject] private IProjectXBusiness ProjectXBusiness { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public int TestInt { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        //protected override async Task OnInitializedAsync()
+        //{
+        //    TestInt = ProjectXBusiness.TestMethod();
+        //}
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
